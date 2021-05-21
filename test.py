4d363626f7e09eb445e12cb74dd09fd9dd08628e
Benjamin Palmer <benpalmer13@hotmail.co.uk>
2021-05-21 10:34:43 +0100

    Cleaned up code and added more comments

--- test.py
+++ test.py
@@ -3,17 +3,16 @@ import csv
 from time import sleep
 from selenium import webdriver
 
+# Open csv to read and convert to a list
 with open(r"C:\Users\benpa\Documents\GitHub\test-python-selenium\website-test.csv", newline='') as f:
     reader = csv.reader(f)
     data = list(reader)
 
+# Initiate list for converting items into strings
 newData = []
-
 for s in data:
     s1 = ''.join(s)
     newData.append(s1)
-
-print(newData)
 
 # Start test case to search python.org
 class PythonOrgsearch(unittest.TestCase):
@@ -22,16 +21,16 @@ class PythonOrgsearch(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
 
-    # Visit webpage, assert it is correct
+    # Visit webpage, wait for 1 second then save a screenshot
     def test_search_in_python_org(self):
         browser = self.browser
         for url in newData:
             browser.get(url)
             sleep(1)
-            filename = 'screenshots/' + url.split('//')[-1] + '.png'
+            filename = 'screenshots/' + url.split('//')[-1] + '.png' # Removes / from the filename
             browser.save_screenshot(filename)
         
-    # Closes the browser
+    # Closes the browser once all webpages are visited
     def tearDown(self):
         self.browser.close()
 
