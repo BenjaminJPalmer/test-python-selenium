7ad29bc8afe0f94f5376645a0767813c749d9053
Benjamin Palmer <benpalmer13@hotmail.co.uk>
2021-07-01 10:45:23 +0100

    Updated error reporting in except cases

--- page.py
+++ page.py
@@ -2,6 +2,7 @@ import os
 import csv
 from seleniumbase import BaseCase
 import requests
+# import pytest_html
 
 # Open csv to read and convert to a list
 with open(r"C:\Users\benpa\Documents\GitHub\test-python-selenium\website-test.csv", newline='') as file:
@@ -21,26 +22,31 @@ class MyTestClass(BaseCase):
 
             # Checks status code is valid
             if urlStatus != 200:
-                print(f"Status code for {url} = {urlStatus}.")
+                print(f"FAIL: Status code for {url} = {urlStatus}.")
+            # If valid proceed
             else:
-                self.open(url)        
-                elements = self.find_elements('li.menu-item a')
+                self.open(url)
+                # Locate links on the page
+                elements = self.find_elements('li a')
                 nav_links = []
+                # Append href links to array
                 for element in elements:
                     nav_links.append(element.get_attribute("href"))
+                # Remove duplicates by putting unique links in new array
                 unique_nav_links = []
-
                 for nav_link in nav_links:
                     if nav_link not in unique_nav_links:
                         unique_nav_links.append(nav_link)
-
+                
                 bad_links = []
                 test_logpath = os.path.join(self.log_path, self.test_id)
+                #TODO update folder naming with iterator for each test run
                 print(f"\n  Screenshot location: {test_logpath}")
 
                 for nav_link in unique_nav_links:
                     try:
                         self.open(nav_link)
+                        # Visual check in case of 404 error pages
                         if self.is_text_visible("404 Error", "h1"):
                             bad_links.append(nav_link)
                             print(f"* Bad page: {nav_link}")
@@ -48,4 +54,5 @@ class MyTestClass(BaseCase):
                             self.save_screenshot_to_logs()
                             print(f"{nav_link} saved.")
                     except:
+                        print(f"Error trying to open {nav_link}.")
                         continue
